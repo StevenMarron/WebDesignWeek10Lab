f9b2a0e8ec28fefc9a3ca038c79306cf733f1cb8
StevenMarron <stevomarron@hotmail.com>
2021-12-01 16:28:18 +0000

    5NumberGuessingGame

---
+++ script/script.js
@@ -0,0 +1,30 @@
+// Week 10 JS
+
+arrayFive=[];
+
+function Array(){
+	for(i=0;i<5; i++){
+		arrayFive.push(Math.floor(Math.random()*10)+1);
+	}	
+}
+
+function viewArray(){
+	alert(arrayFive);
+}
+
+function makeGuess(){
+	var userGuess=prompt("Enter your guess between 1-10");
+	var count=0;
+	for (i=0; i<arrayFive.length; i++){
+		if(arrayFive[i]==userGuess){
+				count=count+1;
+		}
+	}
+	alert("the number "+userGuess+" appeared "+count+" times in the array")
+}
+
+function startOver(){
+	for(i=0;i=arrayFive.length; i++){
+		arrayFive.pop();
+	}
+}
